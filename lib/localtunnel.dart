1ae151fa6dabe4b925a70df7fe04c99f8a6e7355
gb <gb@autanalabs.com>
2023-09-30 12:33:28 -0300

    add tests

--- lib/localtunnel.dart
+++ lib/localtunnel.dart
@@ -1,5 +1,7 @@
 library localtunnel;
 
+export 'src/tunnel.dart' show Tunnel;
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
--- test/localtunnel_test.dart
+++ test/localtunnel_test.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:test/test.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart' as shelf_router;
+import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
 import 'package:localtunnel/localtunnel.dart';
 
@@ -9,4 +16,29 @@ void main() {
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
   });
+
+  test('create a localtunnel', ()  async {
+
+    final router = shelf_router.Router()
+      ..get('/helloworld', helloWorldHandler);
+
+    final server = await shelf_io.serve(
+      shelf.logRequests()
+      .addHandler(router),
+      InternetAddress.anyIPv4, // Allows external connections
+      11978,
+    );
+
+    final lt = Tunnel({
+      'local_host': 'localhost',
+      'local_port', 11978
+    });
+
+    await Future.delayed(const Duration(seconds: 15));
+    server.close(force: true);
+
+  });
 }
+
+shelf.Response helloWorldHandler(shelf.Request request) =>
+    shelf.Response.ok('Hello, World!');
